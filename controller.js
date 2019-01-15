91061e86c5403b9bb49756203cd3b7f48952a0c4
Kyle Leiby <kleiby@etsy.com>
2019-01-14 22:26:47 -0500

    Flesh out more states, distinguish menu background from scene background

--- controller.js
+++ controller.js
@@ -10,23 +10,47 @@ class Controller {
     }
 
     start() {
+        console.log("start");
+
         this.startDay();
     }
 
     startDay() {
+        console.log("startDay");
 
-        // Draw room
-        this._view.fillBackground('brown');
+        const self = this;
 
-        // Draw plants
+        // Draw background
+        this._view.drawRectangle(0, 0, 640, 400, 'brown');
+
         this._plants.forEach(plant => {
             plant.draw(view, () => {
-                console.log("You clicked " + plant.data.name);
+                self.plantMenu(plant);
             });
         });
+
+        this.dayMenu();
+    }
+
+    dayMenu() {
+        console.log("dayMenu");
+
+        // Clear menu background
+        this._view.drawRectangle(0, 400, 640, 80, 'orange');
+
+        // Draw base-level menu buttons
+    }
+
+    plantMenu(plant) {
+        console.log("plantMenu", plant.data.name);
+
+        // Clear menu background
+        this._view.drawRectangle(0, 400, 640, 80, 'orange');
+
+        // Draw plant-level menu buttons
     }
 
     endDay() {
-        // ...
+        console.log("endDay");
     }
 }
